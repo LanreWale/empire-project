f315d6f2b6fcfa7b971f195a12f2524df293bb30
LanreWale <lanreismail15769@gmail.com>
2025-09-11 18:45:21 +0100

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,5 @@
+// /assets/js/config.js
+// SINGLE SOURCE OF TRUTH FOR THE API
+window.EMPIRE_API = {
+  BASE_URL: https://script.google.com/macros/s/AKfycbz-jj7Cr_KzqCku4SQPQ14MEIuCPdq5OEgiiqjo_O2A0FItBrHlmfkoJHViDxuX4P6z/exec
+};

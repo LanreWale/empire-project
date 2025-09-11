790cb4dd9d9b1a380eb7b3f749e504540caaa56f
LanreWale <lanreismail15769@gmail.com>
2025-09-12 00:41:03 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-// asset/js/config.js
+// assets/js/config.js
 window.EMPIRE_API = {
   BASE_URL: "https://script.google.com/macros/s/AKfycbz-jj7Cr_KzqCku4SQPQ14MEIuCPdq5OEgiiqjo_O2A0FItBrHlmfkoJHViDxuX4P6z/exec"
 };

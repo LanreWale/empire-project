62dce884395fae550fbdbcae89cf4bef10a2d9db
LanreWale <lanreismail15769@gmail.com>
2025-09-12 21:22:10 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,3 +2,4 @@
 window.EMPIRE = {
   API_URL: "https://script.google.com/macros/s/AKfycbz-jj7Cr_KzqCku4SQPQ14MEIuCPdq5OEgiiqjo_O2A0FItBrHlmfkoJHViDxuX4P6z/exec"
 };
+<script src="/assets/js/empire-adapter.js?v=1"></script>

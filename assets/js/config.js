45a9da63a971d600dbdab2ccf5bd8b343a2111c9
LanreWale <lanreismail15769@gmail.com>
2025-09-12 21:25:41 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
-// assets/js/config.js
+// /assets/js/config.js
+// Single source of truth for the Empire API endpoint
 window.EMPIRE = {
   API_URL: "https://script.google.com/macros/s/AKfycbz-jj7Cr_KzqCku4SQPQ14MEIuCPdq5OEgiiqjo_O2A0FItBrHlmfkoJHViDxuX4P6z/exec"
 };
-<script src="/assets/js/empire-adapter.js?v=1"></script>

90b45d8fbdc1c78eebd96ca2a0b61b3117297c83
LanreWale <lanreismail15769@gmail.com>
2025-09-12 10:55:08 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,7 @@
-// /assets/js/config.js
+// SINGLE SOURCE OF TRUTH
 window.EMPIRE = {
   API_URL: "https://script.google.com/macros/s/AKfycbzdaxii5JO262nziCmwndM399wryf0TX9RGltFUDVbmuQGht8mcHrPP1FY3ODHdr-iW/exec"
 };
+
+// sanity beacon – remove later
+console.log("[CFG] EMPIRE.API_URL =", window.EMPIRE.API_URL);
